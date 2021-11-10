4ce3e7237539ba5bda41ae833eb97ca5eaab5a3c
Kjell Post <kjell@irstafoto.se>
2021-11-10 14:29:22 +0000

    Shaved off one instruction

--- print.h
+++ print.h
@@ -1,15 +1,15 @@
 .macro KPRINT str
-        B 2f
+        B 1f
 str\@:  .asciz "\str"
         .align 4
-2:      STP X0, X1, [SP, #-16]!
+1:      STP X0, X1, [SP, #-16]!
         STP X2, X16, [SP, #-16]!
         STR LR, [SP, #-16]!
         ADR X0, str\@
         MOV X1, X0
-3:      LDRB W2, [X0], #1
+2:      LDRB W2, [X0], #1
         CMP W2, #0
-        B.NE 3b
+        B.NE 2b
         SUB X2, X0, X1          // len
         SUB X2, X2, #1
         MOV X16, #4             // write
